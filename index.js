6028aa438046fe96510c006d0bb176a64aa8ef2f
wasekamilshaikh <wasekamilshaikh@gmail.com>
2023-12-13 18:43:03 +0530

    This is New

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function Function1()
 {
 alert("In Function1");
-console.log("Log from Function1");
+console.log("Log from Function1. Same Line New.");
 }
 
 async function run()
 {
     console.log("Running...");
+    console.log("Some More");
 }
     
 
